8cc62377865ccdc8b9a332117c9e464a3a49c459
Michael <kotikov.misha@gmail.com>
2018-09-15 13:58:22 +0300

    i tried my best

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let compare = false, counter = 0, strSplit = str.split('');
+
+  for (let i = 0; i < bracketsConfig.length; ++i) {
+    let openBracket = strSplit.indexOf(bracketsConfig[i][0]);
+    let closeBracket = strSplit.indexOf(bracketsConfig[i][1]);
+
+    if (closeBracket < openBracket) {
+      compare = false;
+      break;
+    }
+    for (let j = openBracket; j < strSplit.length; ++j) {
+      if (strSplit[j] === bracketsConfig[i][0]) {
+        counter++;
+      }
+      if (strSplit[j] === bracketsConfig[i][1]) {
+        counter--;
+      }
+    }
+    if (!counter) {
+      compare = true;
+    } else {
+      compare = false;
+    }
+  }
+
+  return compare;
 }
